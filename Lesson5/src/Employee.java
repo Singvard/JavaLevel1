a38f0fafd55c745ab51de7f64bd08f878b98bd7d
Sergey <sergeysaratov@gmail.com>
2022-03-13 11:01:59 +0500

    Переделано под геттеры и сеттеры

--- Lesson5/src/Employee.java
+++ Lesson5/src/Employee.java
@@ -1,20 +1,24 @@
 public class Employee {
 
-    String fullname;
-    String position;
-    String email;
-    int phone;
-    int salary;
-    int age;
+    private String fullname;
+    private String position;
+    private String email;
+    private int phone;
+    private int salary;
+    private int age;
 
-    public Employee(String nameForText, String posForText, String emailForText, int phoneForText, int salaryForText, int ageForText){
-        fullname = nameForText;
-        position = posForText;
-        email = emailForText;
-        phone = phoneForText;
-        salary = salaryForText;
-        age = ageForText;
+    public Employee(String fullname, String position, String email, int phone, int salary, int age){
+        this.fullname = fullname;
+        this.position = position;
+        this.email = email;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
     }
+    public Employee(){
+
+    }
+
     public void tellMeMore() {
         System.out.println("ФИО: " + fullname);
         System.out.println("Должность: " + position);
@@ -23,5 +27,53 @@ public class Employee {
         System.out.println("Зарплата: " + salary);
         System.out.println("Возраст: " + age);
         System.out.println();
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
--- Lesson5/src/Lesson5.java
+++ Lesson5/src/Lesson5.java
@@ -1,15 +1,46 @@
 public class Lesson5 {
 
     public static void main(String[] args) {
-        Employee employee1 = new Employee("Арнольд Карлович Шварценеггер","Терминатор","mr_olympics@mail.ru",1234567890,1000000,55);
 
-        Employee employee2 = new Employee("Чак Чакович Норрис","Бог-Император Человечества","onepunchman@chuck.com",7777777,7777777, 41);
+        Employee employee1 = new Employee();
+        employee1.setFullname("Арнольд Карлович Шварценеггер");
+        employee1.setPosition("Терминатор");
+        employee1.setEmail("mr_olympics@mail.ru");
+        employee1.setPhone(1234567890);
+        employee1.setSalary(1000000);
+        employee1.setAge(55);
 
-        Employee employee3 = new Employee("Сильвестр Андреевич Сталлоне","Рэмбо","real_rambo@vietkong.war",892365219,471942,62);
+        Employee employee2 = new Employee();
+        employee2.setFullname("Чак Чакович Норрис");
+        employee2.setPosition("Бог-Император Человечества");
+        employee2.setEmail("onepunchman@chuck.com");
+        employee2.setPhone(7777777);
+        employee2.setSalary(7777777);
+        employee2.setAge(777);
 
-        Employee employee4 = new Employee("Жан Клодович Вам-Дам","Мистер Шпагат","alcodrugs@former.star",1001101001,9999,40);
+        Employee employee3 = new Employee();
+        employee3.setFullname("Сильвестр Андреевич Сталлоне");
+        employee3.setPosition("Рэмбо");
+        employee3.setEmail("real_rambo@vietkong.war");
+        employee3.setPhone(892365219);
+        employee3.setSalary(471942);
+        employee3.setAge(62);
 
-        Employee employee5 = new Employee("Брюс Лиевич Уиллис","Орешек","hardnuts@alpha.bruce",6660666,56565656,18);
+        Employee employee4 = new Employee();
+        employee4.setFullname("Жан Клодович Вам-Дам");
+        employee4.setPosition("Мистер Шпагат");
+        employee4.setEmail("alcodrugs@former.star");
+        employee4.setPhone(1001101001);
+        employee4.setSalary(9999);
+        employee4.setAge(40);
+
+        Employee employee5 = new Employee();
+        employee4.setFullname("Брюс Лиевич Уиллис");
+        employee4.setPosition("Орешек");
+        employee4.setEmail("hardnuts@alpha.bruce");
+        employee4.setPhone(6660666);
+        employee4.setSalary(56565656);
+        employee4.setAge(18);
 
         Employee[] empArr = new Employee[5];
         empArr[0] = employee1;
@@ -19,7 +50,7 @@ public class Lesson5 {
         empArr[4] = employee5;
 
         for (int i = 0; i < empArr.length; i++){
-            if (empArr[i].age > 40) {
+            if (empArr[i].getAge() > 40) {
                 empArr[i].tellMeMore();
             }
         }
